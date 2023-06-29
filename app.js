26126f746b5084d7c5cb7e74914a8efdce9e4b30
dingbum73 <dingbum73@gmail.com>
2023-06-29 20:21:23 +0800

    feat: add template static files.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+const Resturant = require('./models/resturant')
 
 const port = 3000
 const app = express()
@@ -21,9 +23,23 @@ db.on('error', () => { console.log('DB error') })
 // DB成功連線
 db.once('open', () => { console.log('DB connected!') })
 
+// setting static files
+app.use(express.static('public'))
+
+// setting template engine
+app.engine('hbs', exphbs.create({ defaultLayout: 'main', extname: 'hbs' }).engine)
+app.set('view engine', 'hbs')
+app.set('views', './views')
+
 app.get('/', (req, res) => {
-  res.send('hello world')
+  Resturant.find() // 取出DB resturants的資料 
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(resturants => res.render('index', { resturants }))
+    .catch(error => console.log(error))
+
 })
+
+
 
 app.listen(port, () => {
   console.log(`It's running on http://localhost:${port}`)

b29505d7ab9451fc1e2a149493e29394bd8eb6bb
dingbum73 <dingbum73@gmail.com>
2023-07-16 22:43:05 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes')
 require('./config/mongoose')
 
-const port = 3000
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const port = process.env.PORT
 const app = express()
 
 // setting static files
@@ -15,6 +19,14 @@ app.use(express.static('public'))
 app.engine('hbs', exphbs.create({ defaultLayout: 'main', extname: 'hbs' }).engine)
 app.set('view engine', 'hbs')
 app.set('views', './views')
+
+// setting session
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+
 
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))

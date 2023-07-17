e390c212efa789e2ef0995630f6586363e8dc9ef
dingbum73 <dingbum73@gmail.com>
2023-07-17 08:29:47 +0800

    modify resturantSeeder.js

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 
 
-# 餐廳清單擴充 CRUD 功能
+# 餐廳清單加入登入系統
 第一份CRUD的功能實作專案
 ![image](https://github.com/dingbum73/mypractice/assets/124600894/c055ecd3-d742-4e5e-9797-404c76f1a1d9)
 
@@ -9,6 +9,7 @@
 可以自行輸入，記錄餐廳
 
 ### 功能
+- 登入登出功能
 - 新增一家餐廳
 - 瀏覽一家餐廳的詳細資訊
 - 瀏覽全部所有餐廳
@@ -27,9 +28,16 @@
 
 ## 開發工具
 - Node 18.16.0
-- Express 4.18.2
-- Express-handlebars 7.0.7
 - Boostrap 5.3.0
 - Font-awesome 5.15.4
 - MongoDB
 - mongoose: 7.3.1
+- bcryptjs: 2.4.3
+- connect-flash: 0.1.1,
+- express: 4.18.2,
+- express-handlebars: 7.0.7,
+- express-session: 1.17.1,
+- method-override: 3.0.0,
+- passport: 0.4.1,
+- passport-facebook: 3.0.0,
+- passport-local: 1.0.0
--- models/seeds/resturantSeeder.js
+++ models/seeds/resturantSeeder.js
@@ -1,21 +1,71 @@
+const bcrypt = require('bcryptjs')
 const db = require('../../config/mongoose')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const User = require('../user')
 const Restaurant = require('../restaurant')
 const RestaurantList = require('../restaurantList.json')
 
+const SEED_USER_ONE = {
+  name: 'user1',
+  email: 'user1@example.com',
+  password: '12345678',
+  ownRestaurant: [1, 2, 3] //擁有#1,#2,#3餐廳
+}
+
+const SEED_USER_TWO = {
+  name: 'user2',
+  email: 'user2@example.com',
+  password: '12345678',
+  ownRestaurant: [4, 5, 6]
+}
+
+const seedUsers = [SEED_USER_ONE, SEED_USER_TWO]
+
 
 db.once('open', () => {
-  for (let i = 0; i < RestaurantList.results.length; i++) {
-    Restaurant.create({
-      name: RestaurantList.results[i].name,
-      name_en: RestaurantList.results[i].name_en,
-      category: RestaurantList.results[i].category,
-      image: RestaurantList.results[i].image,
-      location: RestaurantList.results[i].location,
-      phone: RestaurantList.results[i].phone,
-      google_map: RestaurantList.results[i].google_map,
-      rating: RestaurantList.results[i].rating,
-      description: RestaurantList.results[i].description,
+  const seedUsersRun = seedUsers.map(seedUser => {
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(seedUser.password, salt))
+      .then(hash => User.create({
+        name: seedUser.name,
+        email: seedUser.email,
+        password: hash
+      }))
+      .then(user => {
+        const userId = user._id
+        const start = seedUser.ownRestaurant[0]
+        return Promise.all(Array.from(
+          { length: 3 },
+          (value, i) => Restaurant.create({
+            name: RestaurantList.results[start + i].name,
+            name_en: RestaurantList.results[start + i].name_en,
+            category: RestaurantList.results[start + i].category,
+            image: RestaurantList.results[start + i].image,
+            location: RestaurantList.results[start + i].location,
+            phone: RestaurantList.results[start + i].phone,
+            google_map: RestaurantList.results[start + i].google_map,
+            rating: RestaurantList.results[start + i].rating,
+            description: RestaurantList.results[start + i].description,
+            userId
+          })
+        ))
+      })
+      .then(() => {
+        console.log(seedUser.name, 'done')
+      })
+      .catch(err => console.log(err))
+  })
+  Promise.all(seedUsersRun)
+    .then(() => {
+      console.log('All done')
+      process.exit()
     })
-  }
-  console.log('done')
+    .catch(err => console.log(err))
 })
+
+
+
+
